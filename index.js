0b4c56307215adaf3bcf7d8db81ecb9c7c0854f6
Ryan Searls <Ryan.searls@gmail.com>
2021-07-15 14:19:10 -0500

    modern javascript behind the scenes work

---
+++ index.js
@@ -0,0 +1,10 @@
+var moment = require("moment");
+
+console.log("Hello There");
+var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(datetime);
+
+
+let name = "Ryan";
+
+console.log(`How are you today ${name}`);

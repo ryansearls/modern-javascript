ee38911dede2a05838a6be201fd5c2b6372c0f43
Ryan Searls <Ryan.searls@gmail.com>
2022-01-05 14:32:53 -0600

    adds mathjs to console

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var moment = require("moment");
+var math = require("mathjs")
 
 console.log("Hello There");
 var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(datetime);
+
+console.log(math.round(math.e, 3));
 
 
 let name = "Ryan";
